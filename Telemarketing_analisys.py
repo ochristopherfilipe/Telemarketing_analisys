09abb8d6a22e373465cbc6937bedbc6ca92f5030
Christopher Santos <112826117+ochristopherfilipe@users.noreply.github.com>
2023-09-15 16:26:09 -0400

    Update Telemarketing_analisys.py

--- Telemarketing_analisys.py
+++ Telemarketing_analisys.py
@@ -14,7 +14,7 @@ sns.set_theme(style="ticks", rc=custom_params)
 
 
 # Função para ler os dados
-@st.cache
+@st.cache(show_spinner= True, allow_output_mutation=True)
 def load_data(file_data):
     try:
         return pd.read_csv(file_data, sep=';')
@@ -22,7 +22,7 @@ def load_data(file_data):
         return pd.read_excel(file_data)
 
 # Função para filtrar baseado na multiseleção de categorias
-@st.cache
+@st.cache(allow_output_mutation=True)
 def multiselect_filter(relatorio, col, selecionados):
     if 'all' in selecionados:
         return relatorio
